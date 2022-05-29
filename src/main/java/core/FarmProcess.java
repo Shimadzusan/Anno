431675a1539555c347e1815ccb4cb39987d3294f
Shimadzusan <nix_olympica@tut.by>
2022-05-29 16:11:51 +0300

    added logging

--- src/main/java/core/FarmProcess.java
+++ src/main/java/core/FarmProcess.java
@@ -6,19 +6,23 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Properties;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /** здесь описывается только последовательность и логика воздействий, основного процесса*/
 public class FarmProcess extends Thread implements Runnable {
 	int frequency;
 	int quantity;
+	private static final Logger LOG = LogManager.getLogger(FarmProcess.class);
+	
 	public FarmProcess() throws IOException {
+		LOG.info("This is my log from FarmProcess");
 		Properties props = new Properties();
 	        try(InputStream in = Files.newInputStream(Paths.get("configuration.properties"))){
 	            props.load(in);
 	        }
 	        frequency = Integer.parseInt(props.getProperty("frequency"));
 	        quantity = Integer.parseInt(props.getProperty("quantity"));
-//	        String username = props.getProperty("username");
-//	        String password = props.getProperty("password");
 	        System.out.println(frequency + " / " + quantity);
 	}
 
