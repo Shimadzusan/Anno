b58bcf9e61a99ad7b9b8c3a5b292bda6916b0470
Shimadzusan <nix_olympica@tut.by>
2022-05-29 15:25:22 +0300

    added new branches workforce, transport, added properties to FarmProcess

--- src/main/java/core/FarmProcess.java
+++ src/main/java/core/FarmProcess.java
@@ -1,13 +1,32 @@
 package core;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
+
 /** здесь описывается только последовательность и логика воздействий, основного процесса*/
 public class FarmProcess extends Thread implements Runnable {
+	int frequency;
+	int quantity;
+	public FarmProcess() throws IOException {
+		Properties props = new Properties();
+	        try(InputStream in = Files.newInputStream(Paths.get("configuration.properties"))){
+	            props.load(in);
+	        }
+	        frequency = Integer.parseInt(props.getProperty("frequency"));
+	        quantity = Integer.parseInt(props.getProperty("quantity"));
+//	        String username = props.getProperty("username");
+//	        String password = props.getProperty("password");
+	        System.out.println(frequency + " / " + quantity);
+	}
 
 	@Override
 	public void run() {
 		try {
-			for (int j = 0; j < 100; j++) {
-				Thread.sleep(2500);
+			for (int j = 0; j < quantity; j++) {
+				Thread.sleep(frequency);
 				initLogicStreamExecution();	//..some stream execution
 			}
 		} catch (InterruptedException e) {
