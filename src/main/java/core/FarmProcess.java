1d27dce591f36babc5b256d507bac4877faf355d
Shimadzusan <nix_olympica@tut.by>
2022-05-31 09:25:06 +0300

    created modules for Configuration and Logging

--- src/main/java/core/FarmProcess.java
+++ src/main/java/core/FarmProcess.java
@@ -11,32 +11,25 @@ import org.apache.logging.log4j.Logger;
 
 /** здесь описывается только последовательность и логика воздействий, основного процесса*/
 public class FarmProcess extends Thread implements Runnable {
-	int frequency;
-	int quantity;
+
 	private static final Logger LOG = LogManager.getLogger(FarmProcess.class);
 	
 	public FarmProcess() throws IOException {
-		LOG.info("This is my log from FarmProcess");
-		Properties props = new Properties();
-	        try(InputStream in = Files.newInputStream(Paths.get("configuration.properties"))){
-	            props.load(in);
-	        }
-	        frequency = Integer.parseInt(props.getProperty("frequency"));
-	        quantity = Integer.parseInt(props.getProperty("quantity"));
-	        System.out.println(frequency + " / " + quantity);
+
 	}
 
 	@Override
 	public void run() {
+		LOG.info("begin FarmProcess");
 		try {
-			for (int j = 0; j < quantity; j++) {
-				Thread.sleep(frequency);
+			for (int j = 0; j < ConfigurationModule.quantity; j++) {
+				Thread.sleep(ConfigurationModule.frequency);
 				initLogicStreamExecution();	//..some stream execution
 			}
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		
+		LOG.info("end FarmProcess");
 	}
 
 	 /** некоторое логическое воздействие на объект Farm*/
